6e65c3d6e78a65876fbdca23316fe499efabac90
Zsombi55 <47913849+Zsombi55@users.noreply.github.com>
2019-03-09 11:32:01 +0200

    Button value to textbox.
    Did not manage to make the adding of the html button tag values to the input type text values with for loop. But made one of them work with the simple but ultimately longer method of making a function for each button.

--- functions.js
+++ functions.js
@@ -1,12 +1,10 @@
 
 var ipOp = document.getElementById("inputOutput");
 var formDisp = document.getElementById("formulaDisplay");
-var btnValue = document.getElementsByClassName("nrBtn").value;
-
+/*
 console.info(ipOp);
 console.info(formDisp);
 
-console.info(btnValue);	// result: undefined
 console.info(document.getElementById("seven").value);	// result: 7 , undefined
 // likely eror & undefined cause is that the getElementsByClassName returns an array of objects not a single object.
 
@@ -21,6 +19,29 @@ console.info(inputValue());
 
 document.getElementsByClassName("nrBtn").onclick = function () {
 	inputValue();
+}*/
+
+/* function inputValue() {
+	var nrButtons = document.getElementsByClassName("nrBtn");
+	for(var i = 0; i < nrButtons.length; i++) {
+		nrButtons[i].onclick = function() {
+			// ipOp.value += nrButtons[i].value;
+			document.getElementById("inputOutput").value += nrButtons[i].value;
+		}
+	}
+}
+
+document.getElementById("seven").onclick = function() {
+	inputValue();
+} */
+
+function clickSeven() {
+	ipOp.value += document.getElementById("seven").value;
+}
+
+document.getElementById("seven").onclick = function() {
+	clickSeven();
+	console.info(ipOp.value);
 }
 
 
