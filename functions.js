e8417e2bb5962ff7696690b29dbc3a28ff95fc89
Zsombi55 <47913849+Zsombi55@users.noreply.github.com>
2019-03-10 11:12:07 +0200

    Version 1 done.
    The expected basic functions of a simple calculator work as expected. It is not clean, some events are not being handles, such as replacing the last equation's result with the new equation's first number instead of appending to it.
    
    Since using eval() is too resource intensive for such simple calculations the next step will be making my own easier-on-the-system calculator functions. Along the way some of the currently missing events may also be implemented, however that may just as well remain to be done afterwards instead.

--- functions.js
+++ functions.js
@@ -24,50 +24,82 @@ document.getElementById("seven").onclick = function() {
 // ----- Put the button values into the text fields. Numbers & symbols. -----
 // Numbers.
 document.getElementById("one").onclick = function() {
-	formDisp.value += document.getElementById("one").value;
-	ipOp.value += document.getElementById("one").value;
+	if(ipOp.value == 0) {
+		firstNotZero(document.getElementById("one").value);
+	} else {
+		formDisp.value += document.getElementById("one").value;
+		ipOp.value += document.getElementById("one").value;
+	}
 	console.info("Second box, inputOutput: " + ipOp.value);
 }
 
 document.getElementById("two").onclick = function() {
-	formDisp.value += document.getElementById("two").value;
-	ipOp.value += document.getElementById("two").value;
+	if(ipOp.value == 0) {
+		firstNotZero(document.getElementById("two").value);
+	} else {
+		formDisp.value += document.getElementById("two").value;
+		ipOp.value += document.getElementById("two").value;
+	}
 	console.info("Second box, inputOutput: " + ipOp.value);
 }
 
 document.getElementById("three").onclick = function() {
-	formDisp.value += document.getElementById("three").value;
-	ipOp.value += document.getElementById("three").value;
+	if(ipOp.value == 0) {
+		firstNotZero(document.getElementById("three").value);
+	} else {
+		formDisp.value += document.getElementById("three").value;
+		ipOp.value += document.getElementById("three").value;
+	}
 	console.info("Second box, inputOutput: " + ipOp.value);
 }
 
 document.getElementById("four").onclick = function() {
-	formDisp.value += document.getElementById("four").value;
-	ipOp.value += document.getElementById("four").value;
+	if(ipOp.value == 0) {
+		firstNotZero(document.getElementById("four").value);
+	} else {
+		formDisp.value += document.getElementById("four").value;
+		ipOp.value += document.getElementById("four").value;
+	}
 	console.info("Second box, inputOutput: " + ipOp.value);
 }
 
 document.getElementById("five").onclick = function() {
-	formDisp.value += document.getElementById("five").value;
-	ipOp.value += document.getElementById("five").value;
+	if(ipOp.value == 0) {
+		firstNotZero(document.getElementById("five").value);
+	} else {
+		formDisp.value += document.getElementById("five").value;
+		ipOp.value += document.getElementById("five").value;
+	}
 	console.info("Second box, inputOutput: " + ipOp.value);
 }
 
 document.getElementById("six").onclick = function() {
-	formDisp.value += document.getElementById("six").value;
-	ipOp.value += document.getElementById("six").value;
+	if(ipOp.value == 0) {
+		firstNotZero(document.getElementById("six").value);
+	} else {
+		formDisp.value += document.getElementById("six").value;
+		ipOp.value += document.getElementById("six").value;
+	}
 	console.info("Second box, inputOutput: " + ipOp.value);
 }
 
 document.getElementById("seven").onclick = function() {
-	formDisp.value += document.getElementById("seven").value;
-	ipOp.value += document.getElementById("seven").value;
+	if(ipOp.value == 0) {
+		firstNotZero(document.getElementById("seven").value);
+	} else {
+		formDisp.value += document.getElementById("seven").value;
+		ipOp.value += document.getElementById("seven").value;
+	}
 	console.info("Second box, inputOutput: " + ipOp.value);
 }
 
 document.getElementById("eight").onclick = function() {
-	formDisp.value += document.getElementById("eight").value;
-	ipOp.value += document.getElementById("eight").value;
+	if(ipOp.value == 0) {
+		firstNotZero(document.getElementById("eight").value);
+	} else {
+		formDisp.value += document.getElementById("eight").value;
+		ipOp.value += document.getElementById("eight").value;
+	}
 	console.info("Second box, inputOutput: " + ipOp.value);
 }
 
@@ -82,19 +114,32 @@ document.getElementById("nine").onclick = function() {	// -- modify the others t
 }
 
 document.getElementById("zero").onclick = function() {
-	formDisp.value += document.getElementById("zero").value;
-	ipOp.value += document.getElementById("zero").value;
+	if(ipOp.value == 0) {
+		console.log("The first digit is already a zero!");
+	} else {
+		formDisp.value += document.getElementById("zero").value;
+		ipOp.value += document.getElementById("zero").value;
+	}
 	console.info("Second box, inputOutput: " + ipOp.value);
 }
 
 document.getElementById("doubleZero").onclick = function() {
-	formDisp.value += document.getElementById("doubleZero").value;
-	ipOp.value += document.getElementById("doubleZero").value;
+	if(ipOp.value == 0) {
+		console.log("The first digit is already a zero!");
+	} else {
+		formDisp.value += document.getElementById("doubleZero").value;
+		ipOp.value += document.getElementById("doubleZero").value;
+	}
 	console.info("Second box, inputOutput: " + ipOp.value);
 }
 
 // Symbols: manual.
 document.getElementById("decimal").onclick = function() { 
+	if(ipOp.value == 0) {
+		ipOp.value += document.getElementById("decimal").value;
+		formDisp.value += 0 + ipOp.value;
+	}
+	
 	if(!ipOp.value.includes(",")){	// -- make a generic support function for the operator buttons too.
 		ipOp.value += document.getElementById("decimal").value;
 	} else {
@@ -144,7 +189,27 @@ document.getElementById("plus").onclick = function() {
 	nrValue = formDisp.value;
 	addNr(nrValue);
 	console.info("First box, formulaDisplay: " + formDisp.value + "\n" + "Second box, inputOutput: " + ipOp.value);
-	
+}
+
+document.getElementById("minus").onclick = function() {
+	formDisp.value += document.getElementById("minus").value;
+	nrValue = formDisp.value;
+	deductNr(nrValue);
+	console.info("First box, formulaDisplay: " + formDisp.value + "\n" + "Second box, inputOutput: " + ipOp.value);
+}
+
+document.getElementById("multiply").onclick = function() {
+	formDisp.value += document.getElementById("multiply").value;
+	nrValue = formDisp.value;
+	deductNr(nrValue);
+	console.info("First box, formulaDisplay: " + formDisp.value + "\n" + "Second box, inputOutput: " + ipOp.value);
+}
+
+document.getElementById("divide").onclick = function() {
+	formDisp.value += document.getElementById("divide").value;
+	nrValue = formDisp.value;
+	deductNr(nrValue);
+	console.info("First box, formulaDisplay: " + formDisp.value + "\n" + "Second box, inputOutput: " + ipOp.value);
 }
 
 document.getElementById("equal").onclick = function() {
@@ -153,18 +218,33 @@ document.getElementById("equal").onclick = function() {
 	formDisp.value += document.getElementById("equal").value + ipOp.value;
 	console.info("First box, formulaDisplay: " + formDisp.value + "\n" + "Second box, inputOutput: " + ipOp.value);
 }
-// ----- . -----
 
 // ----- Operator Support Functions. -----
+var eqStr;
+
 function addNr(eqStr) {		// make a generic equation calculator, if can't then make one like this for each separately.
-	var eqStr = formDisp.value + 0;	// Equation string: numbers, decimal commas, thousand delimiter points & operators.
+	eqStr = 0 + formDisp.value;	// Equation string: numbers, decimal commas, thousand points & operators.
+	ipOp.value = eval(eqStr);
+}
+
+function deductNr(eqStr) {		// allow non-"equal" operators to be first characters without evaluating the equation.
+	eqStr = 0 + formDisp.value;
+	ipOp.value = eval(eqStr);
+}
+
+function multiplyNr(eqStr) {
+	eqStr = 0 + formDisp.value;
+	ipOp.value = eval(eqStr);
+}
+
+function divideNr(eqStr) {
+	eqStr = 0 + formDisp.value;
 	ipOp.value = eval(eqStr);
 }
 
 function finishEquation (eqStr) {
 	ipOp.value = eval(eqStr);
 }
-// ----- . -----
 
 // ----- Support Functions -----
 function lastKeyPress() {	// ----- TO DO --- !!!!!!
@@ -172,6 +252,7 @@ function lastKeyPress() {	// ----- TO DO --- !!!!!!
 }
 
 function firstNotZero(firstNr) {	// Switch first digit if it's not the default "0".
+	formDisp.value = firstNr;
 	ipOp.value = firstNr;
 }
 
@@ -184,4 +265,6 @@ function firstNotZero(firstNr) {	// Switch first digit if it's not the default "
 	- or pressing it repeatedly after having it done so just before "this pressing event".
 
 > Make a "key tracker", use it to change the ipOp value instead of appending to it if pressing a number after the "equal".
+
+> Add a side panel where warning messages and finished equations are to be displayed, like a "session history".
 */
